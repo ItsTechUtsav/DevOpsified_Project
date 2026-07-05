cb9a2367437e88951e14a84219515a0902f023a5
ItsTechUtsav <utsavsh.01@gmail.com>
2026-07-05 07:39:11 +0530

    Check workflow #1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello DevOps!");
+    res.send("Welcome to DevOps!");
 });
 
 app.get("/health", (req, res) => {
